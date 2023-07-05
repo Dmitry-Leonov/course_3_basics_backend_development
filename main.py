5e4d7dec09dc5118bd3344807d795a37b6a53060
Dmitry Leonov <leonov.do72@gmail.com>
2023-07-06 00:29:51 +0300

    функции sort_by_date

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from utils import load_operation
+from utils import load_operation, sort_by_date
 
 if __name__ == '__main__':
     filename = 'operations.json'
     card_transactions = load_operation(filename)
-    print(card_transactions)
+    sorted_card_transactions = sort_by_date(card_transactions)
+    for i in sorted_card_transactions:
+        print(i)
 
--- utils.py
+++ utils.py
@@ -1,7 +1,25 @@
 import json
+import operator
+from typing import List, Any
 
 
 def load_operation(filename: str) -> list:
+    """
+    Функция получения списка словарей из файла
+    :param filename: имя файла
+    :return: список словарей
+    """
     with open(filename, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        return data
+        operation: list = json.load(f)
+    return operation
+
+
+def sort_by_date(data) -> list:
+    """
+    Функция возвращает послдение 5 транзакций отсротированных по дате
+    :param data: список транзакций (словари)
+    :return: список последних 5 транзакци
+    """
+    data_new = [i for i in data if i.get('date') and i['state'] == 'EXECUTED']
+    sorted_data_new = sorted(data_new, key=operator.itemgetter('date'), reverse=True)
+    return sorted_data_new[:5]
